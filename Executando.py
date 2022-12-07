7739de752e63d8bd26f0ceb4bdade7a43821177e
RennanAtes <RennanAugustoBR@gmail.com>
2022-12-07 12:27:00 -0300

    Funções
    Funções que podem ser chamadas através das palavras chaves, elas são as responsáveis por abrir o arquivo ,fechar, pesquisar, entre outras coisas.

--- Executando.py
+++ Executando.py
@@ -12,3 +12,15 @@ def Executar(Swan,chave,SwanFala):
         abrir(Swan)
     elif chave == "fechar":
         fechar(Swan)
+def pesquisa(SwanFala):
+    print ("Função pesquisa chamada.")
+    webbrowser.get('windows-default').open_new('https://www.google.com.br/search?q=' + f'{SwanFala}')
+def youtube(SwanFala):
+    print ("função youtube chamada")
+    webbrowser.get('windows-default').open_new('https://www.youtube.com/results?search_query=' + f'{SwanFala}')
+def abrir(Swan):
+    if 'google' in Swan:
+        subprocess.call([google])
+def fechar(Swan):
+    if 'google' in Swan:
+        subprocess.call(["taskkill", "/F", "/IM", "chrome.exe"])
